c7fd5db874ea8a8ce19f8c9d5d83b67acc86bebc
Nino Chimento <saninoc84@gmail.com>
2020-01-04 18:41:13 +0100

    movimentazione sipario

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-
+document.getElementById('play').addEventListener("click",
+function () {
+  document.getElementById('sipariotop').classList.remove("hidden");
+  document.getElementById('sipariosx').classList.remove("hidden");
+  document.getElementById('sipariodx').classList.remove("hidden");
+  document.getElementById('play').classList.add("hidden");
+  setTimeout(function () {
+    document.getElementById('sipariosx').classList.add("hidden");
+    document.getElementById('sipariodx').classList.add("hidden");
+    document.getElementById('sipariotop').classList.add("hidden");
+  }, 4000);
+}
+);
